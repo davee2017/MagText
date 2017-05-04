607fb776d7beb734889cec3a6409a86dae357db1
David Ellison <s3288400@student.rmit.edu.au>
2017-05-04 10:14:12 +1000

    Least colour value determined
    Determined least occurring colour value correctly on macOS. Now will
    remove debug statements within function.

---
+++ picproc-leastcolval.js
@@ -0,0 +1,59 @@
+/*  MagText - An application to produce text and speech from content in the 
+    surrounding environment under any lighting condition. 
+    Copyright (C) 2017 David Ellison
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Least colour value
+// Determines the least occurring colour in a picture.
+// Accepts a picture canvas.
+
+function leastColVal(picCanvas)
+{
+   // Get canvas 2D context
+   var context = picCanvas.getContext('2d');
+   
+   // Get pixels from canvas
+   var picPixels = context.getImageData(0, 0, picCanvas.width, picCanvas.height);
+   
+   // Get pixel values 
+   var redVal = -1;
+   var pixelVals = [];
+   for (var redPos = 0; redPos < picPixels.data.length; redPos += 4)
+   {
+      // Get pixel red value
+      redVal = picPixels.data[redPos];
+      pixelVals.push(redVal); 
+   }
+
+   // Compile histogram
+   var uniqPxVals = -1;                      // Unique pixel values
+   uniqPxVals = uniqVals(pixelVals);         
+   var valCounts = -1;                       // Unique pixel value amounts
+   valCounts = cntVals(pixelVals, uniqPxVals);
+   console.log("Counts of colour values: "); // Debug
+   for (var pos = 0; pos < uniqPxVals.length; pos += 1)
+   {
+      console.log("Pos ", pos, "Val ", uniqPxVals[pos], "Cnt ", valCounts[pos]);
+   }
+   
+   // Determine least occurring colour value
+   var minCount = Math.min.apply(null, valCounts);
+   var minCountPos = valCounts.indexOf(minCount);
+   var leastVal = uniqPxVals[minCountPos];
+   console.log("Least col val in leastColVal(): ", leastVal);
+   
+   return leastVal;
+}
