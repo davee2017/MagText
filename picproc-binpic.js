0703edb644da1c6cc208974471d3275ce0a5f309
David Ellison <s3288400@student.rmit.edu.au>
2017-05-06 17:16:35 +1000

    Ideal val debug removed
    Removed debug of ideal values from binPic(). Will now play with ideal
    values on Android tablet. Subsequent development will hopefully now be
    on turning this into a user-friendly web application.

--- picproc-binpic.js
+++ picproc-binpic.js
@@ -25,9 +25,6 @@
 //
 function binPic(picCanvas, minWhite, maxWhite)
 {
-   console.log("Min white col val: ", minWhite);
-   console.log("Max white col val: ", maxWhite);
-   
    // Get canvas 2D context
    var context = picCanvas.getContext('2d');
    
